a2a82ef3adfb997a9c1a42c9fdf315400f6156ad
Adithya <adithya.sekar@tristhaglobal.com>
2021-03-23 23:25:39 +0530

    Youtube UI changes

--- src/app/youtube-api/youtube-api.component.ts
+++ src/app/youtube-api/youtube-api.component.ts
@@ -3,7 +3,7 @@ import { Subject } from 'rxjs';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { YoutubeService } from '../youtube.service';
 import { takeUntil } from 'rxjs/operators';
-import { NgImageSliderComponent } from 'ng-image-slider';
+
 declare var $: any;
 
 @Component({
@@ -13,7 +13,7 @@ declare var $: any;
 })
 export class YoutubeApiComponent implements OnInit {
   videos: any[];
-  @ViewChild('nav') slider: NgImageSliderComponent;
+ 
   private unsubscribe$: Subject<any> = new Subject();
   constructor(private spinner: NgxSpinnerService, private youTubeService: YoutubeService) { }
 
@@ -21,6 +21,7 @@ export class YoutubeApiComponent implements OnInit {
     this.spinner.show()
     setTimeout(()=>
     {
+      console.log("youtube API")
       this.spinner.hide()
       $('.owl-carousel').owlCarousel({
         loop: false,
@@ -44,67 +45,15 @@ export class YoutubeApiComponent implements OnInit {
     },1000)
     this.videos = [];
     this.youTubeService
-      .getVideosForChanel('UCxtMsJZRzwi571js0zoICUw', 15)
+      .getVideosForChanel('UCxtMsJZRzwi571js0zoICUw', 15) 
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe(lists => {
+        console.log(lists, "lists")
         for (let element of lists["items"]) {
           this.videos.push(element)
           // this.videos.push({video:`https://youtu.be/${element.id.videoId}`,title:element.snippet.title,alt:'youtube video'})
         }
-      });
-     
-    //     setTimeout(()=>
-    // {
-    //   this.spinner.hide()
-    //   if(this.slider.sliderPrevDisable)
-    //   {
-    //     $('#prevclick').prop('disabled', true).css('opacity', '0.5')
-    //     $('#nextclick').prop('disabled', false).css('opacity', '1')
-    //   }
-    
-    // },500)
-    
+      });  
   }
-
-  prevImageClick() {
-    console.log("this.slider", this.slider)
-    this.slider.prev();
-    setTimeout(()=>
-    {
-    if(this.slider.sliderPrevDisable)
-    {
-      $('#prevclick').prop('disabled', true).css('opacity', '0.5')
-      $('#nextclick').prop('disabled', false).css('opacity', '1')
-    }
-    else
-    {
-      console.log('qwertyuik')
-      $('#prevclick').prop('disabled', false).css('opacity', '1')
-      $('#nextclick').prop('disabled', false).css('opacity', '1')
-    
-    }
-  },1000)
- 
-    
-}
-
-nextImageClick() {
-    this.slider.next();
-    setTimeout(()=>
-    {
-    if(this.slider.sliderNextDisable)
-    {
-      $('#prevclick').prop('disabled', false).css('opacity', '1')
-      $('#nextclick').prop('disabled', true).css('opacity', '0.5')
-    }
-    else
-    {
-      $('#prevclick').prop('disabled', false).css('opacity', '1')
-      $('#nextclick').prop('disabled', false).css('opacity', '1')
-    }
-  },1000)
-   
-}
-
 }
 //UCxtMsJZRzwi571js0zoICUw
